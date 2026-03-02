f8395e8eda58f0a0c5019087fa7d3ae8fe9e9456
mariadanilova-ship-it <maria.danilova@exactprosystems.com>
2026-03-02 23:36:22 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig } from "vite";
+ import { defineConfig } from "vite";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/Match3game/",
-  plugins: [react(), tailwindcss()],
+
+  plugins: [
+    // React + Tailwind нужны для Make
+    react(),
+    tailwindcss(),
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   assetsInclude: ["**/*.svg", "**/*.csv"],
 });
